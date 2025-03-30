8da2645cbc340dedd6e51ec1ed6adf3b1c053973
Luke Watson-Davies <lukew@cogapp.com>
2025-03-30 23:28:35 +0100

    1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { Elysia } from "elysia";
+import {Elysia} from "elysia";
 
 const app = new Elysia()
-  .get("/", () => "Hello from Elysia API!")
+  .get("/", () => "Hello from Elysia API 1")
   .listen(Bun.env.APP_PORT || 3000);
 
 console.log(`🦊 Elysia is running on http://localhost:${app.server?.port}`);
